3fa8e9d236167bd38d8d9897105d5621ee0cce88
pc-magas <pc-magas@users.noreply.github.com>
2018-01-28 18:47:15 +0200

    Add colors

--- App/App.cpp
+++ App/App.cpp
@@ -6,7 +6,11 @@
 #include <cstdlib>
 #include <string.h>
 #include <iostream>
-using namespace std;
+
+const char RED[8]="\033[0;31m";
+const char GREEN[8]="\033[0;32m";
+const char YELLOW[8]="\033[0;33m";
+const char NO_COLOR[7]="\033[0m";
 
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
@@ -17,15 +21,14 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    int result,a=2, b=1, g=25;
-
+    int result;
     char c;
     char pass[25];
-    char pin[25];
 
-    std::cout << "" << std::endl;
+    std::cout << YELLOW << "Generating a key:"<<NO_COLOR;
 
     for (int i; i<=25; i++)  {
+        std::cout<< '.';
         sgx_status_t status2 = hello (global_eid, &result, &c);
         pass[i] = c;
         if (status2 != SGX_SUCCESS) {
@@ -34,9 +37,13 @@ int main(int argc, char const *argv[]) {
         }
     }
 
+    //Terminating the color and displaying done
+    std::cout<< GREEN << "DONE"<< NO_COLOR << std::endl;
+
+    std::cout<<"GANARATED KEY: "<< GREEN;
     for (int i = 25; i>=0; i--){
-      cout<< pass[i];
+      std::cout<< pass[i];
     }
-    std::cout << "\n \n \n " << std::endl;
+    std::cout <<NO_COLOR<< std::endl;
     return 0;
 }
