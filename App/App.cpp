cc04f8faf2a5c5d1b8d406893664778679287895
pc-magas <pc-magas@users.noreply.github.com>
2018-01-28 18:24:17 +0200

    Progress so far

--- App/App.cpp
+++ App/App.cpp
@@ -2,6 +2,11 @@
 #include "Enclave_u.h"
 #include "sgx_urts.h"
 #include "sgx_utils/sgx_utils.h"
+#include <string.h>
+#include <cstdlib>
+#include <string.h>
+#include <iostream>
+using namespace std;
 
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
@@ -12,18 +17,37 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    int result=0,a=1,b=2;
+    int result,a=2, b=1, g=25;
+
+    char c;
+    char pass[25];
+    char pin[25];
 
     std::cout << "Calling secure adding" << std::endl;
     sgx_status_t status = sum(global_eid, &a,&b,&result);
+    for (int i; i<=25; i++)
 
+    {sgx_status_t status2 = hello (global_eid, &result, &c);
+        pass[i] = c;
+
+        //cout << "piou";
+
+    }
+    //sgx_status_t status3 = keyGen (global_eid, pin, pass);
     std::cout << "Enclave Returned status: " << std::hex << status << std::endl;
 
     if (status != SGX_SUCCESS) {
         std::cerr << "Enclave have not called sucessfully" << std::endl;
         return 1;
     }
+    //std::cout << "EDO" << '\n';
+    for (int i = 25; i>=0; i--){
+      cout<< pass[i];
+    }
+    std::cout << "\n \n \n Sum result: " << result << std::endl;
+    //std::cout << "Hello Result: %s "<< c << std::endl;
+    //std::cout << "Your Pass fff: "<< pass << std::endl;
+    //printf("Your Pass fff: %s\n", pass);
 
-    std::cout << "Sum result: " << result << std::endl;
     return 0;
 }
--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -1,6 +1,18 @@
 #include "Enclave_t.h"
+#include "sgx_trts.h"
+#include "sgx_urts.h"
+#include <string.h>
+#include <cstdlib>
+#include <string.h>
 
-void sum(int *a, int *b, int *sum)
-{
-  *sum = *a + *b;
+using namespace std;
+
+
+void hello(int *a, char *c){
+  int pass2;
+  char f[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','4','7','8'};
+  uint32_t val;
+  sgx_read_rand((unsigned char *) &val, 4);
+  pass2 = val % strlen(f) ;
+  *c = f[pass2];
 }
